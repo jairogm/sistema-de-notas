f9900a970b4d2ab7c6337e457a6ad34d49e0af36
Jhon Garces Montes <jgarces@lendingpoint.com>
2023-03-17 22:11:23 -0500

    finished all the  functionality for the buttons

--- App.js
+++ App.js
@@ -1,149 +1,165 @@
+//react
+import { useEffect } from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-import { Formik } from 'formik';
-import { useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-web';
+//Formik
+import { useFormik } from 'formik';
+
+//Styles
 import styles from './App.styles'
-import { calcularDefinitiva } from './helpers';
-import { formValidationSchema } from './schemas';
-export default function App() {
 
-  const handleSubmit = (values) => {
-    console.log('2', values)
-    const notaDefinitiva = calcularDefinitiva(values.notaUno, values.notaDos, values.notaTres)
-    console.log({
-      ...values,
-      definitiva: notaDefinitiva
-    })
+//Helpers
+import { buscarById, calcularDefinitiva, init } from './helpers';
+
+//Schemas
+import { formValidationSchema } from './schemas';
+
+export default function App() {
+  const alumnos = JSON.parse(localStorage.getItem('alumnos'))
+  const formikProps = useFormik({
+    initialValues: init,
+    validationSchema: formValidationSchema,
+    onSubmit: handleSubmit
+  });
+
+  const handleSubmit = () => {
+    const notaDefinitiva = calcularDefinitiva(formikProps.values.notaUno, formikProps.values.notaDos, formikProps.values.notaTres).toFixed(2)
+    // formikProps.resetForm()
+    formikProps.setFieldValue('definitiva', notaDefinitiva)
+    localStorage.setItem('alumnos', JSON.stringify([...alumnos, { ...formikProps.values, definitiva: notaDefinitiva }]))
   }
+
+  const search = (identificacion) => {
+    const alumno = buscarById(identificacion)
+    formikProps.setValues(alumno)
+  }
+
+  useEffect(() => {
+    if (!localStorage.getItem('alumnos')) {
+      localStorage.setItem('alumnos', JSON.stringify([]))
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Sistema de Notas</Text>
 
-      <Formik
-        validationSchema={formValidationSchema}
-        initialValues={{ identificacion: '', nombres: '', asignatura: '', notaUno: '', notaDos: '', notaTres: '', definitiva: '', observacion: '' }}
-        onSubmit={values => handleSubmit(values)}
-      >
-        {({
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          values,
-          errors,
-          touched,
-          isValid,
-          setFieldValue
-        }) => (
-          <>
-            <Text style={styles.title}>Sistema de Notas</Text>
-            <View style={styles.formItem}>
-              <Text style={styles.formLabel}>Identificacion</Text>
-              <TextInput
-                style={styles.formInput}
-                name="identificacion"
-                onChangeText={handleChange('identificacion')}
-                onBlur={handleBlur('identificacion')}
-                value={values.identificacion}
-                keyboardType="text"
-              />
-            </View>
-            {(errors.identificacion && touched.identificacion) &&
-              <Text style={styles.errorText}>{errors.identificacion}</Text>
-            }
-            <View style={styles.formItem}>
-              <Text style={styles.formLabel}>Nombres</Text>
-              <TextInput
-                style={styles.formInput}
-                name="nombres"
-                onChangeText={handleChange('nombres')}
-                onBlur={handleBlur('nombres')}
-                value={values.nombres}
-                keyboardType="text"
-              />
-            </View>
-            {(errors.nombres && touched.nombres) &&
-              <Text style={styles.errorText}>{errors.nombres}</Text>
-            }
-            <View style={styles.formItem}>
-              <Text style={styles.formLabel}>Asignatura</Text>
-              <TextInput
-                style={styles.formInput}
-                name="asignatura"
-                onChangeText={handleChange('asignatura')}
-                onBlur={handleBlur('asignatura')}
-                value={values.asignatura}
-                keyboardType="text"
-              />
-            </View>
-            {(errors.asignatura && touched.asignatura) &&
-              <Text style={styles.errorText}>{errors.asignatura}</Text>
-            }
-            <View style={styles.formItem}>
-              <Text style={styles.formLabel}>Nota Momento 1 (30%)</Text>
-              <TextInput
-                style={styles.formInput}
-                name="notaUno"
-                onChangeText={handleChange('notaUno')}
-                onBlur={handleBlur('notaUno')}
-                value={values.notaUno}
-                keyboardType="text"
-              />
-            </View>
-            {(errors.notaUno && touched.notaUno) &&
-              <Text style={styles.errorText}>{errors.notaUno}</Text>
-            }
-            <View style={styles.formItem}>
-              <Text style={styles.formLabel}>Nota Momento 2 (35%)</Text>
-              <TextInput
-                style={styles.formInput}
-                name="notaDos"
-                onChangeText={handleChange('notaDos')}
-                onBlur={handleBlur('notaDos')}
-                value={values.notaDos}
-                keyboardType="text"
-              />
-            </View>
-            {(errors.notaDos && touched.notaDos) &&
-              <Text style={styles.errorText}>{errors.notaDos}</Text>
-            }
-            <View style={styles.formItem}>
-              <Text style={styles.formLabel}>Nota Momento 3 (35%)</Text>
-              <TextInput
-                style={styles.formInput}
-                name="notaTres"
-                onChangeText={handleChange('notaTres')}
-                onBlur={handleBlur('notaTres')}
-                value={values.notaTres}
-                keyboardType="text"
-              />
-            </View>
-            {(errors.notaTres && touched.notaTres) &&
-              <Text style={styles.errorText}>{errors.notaTres}</Text>
-            }
-            <View style={styles.formItem}>
-              <Text style={styles.formLabel}>Definitiva</Text>
-              <TextInput style={styles.formInput} />
-            </View>
-            <View style={styles.formItem}>
-              <Text style={styles.formLabel}>Observacion (35%)</Text>
-              <TextInput style={styles.formInput} />
-            </View>
+      <View style={styles.formItem}>
+        <Text style={styles.formLabel}>Identificacion</Text>
+        <TextInput
+          style={styles.formInput}
+          name="identificacion"
+          onChangeText={formikProps.handleChange('identificacion')}
+          onBlur={formikProps.handleBlur('identificacion')}
+          value={formikProps.values.identificacion}
+          keyboardType="text"
+        />
+      </View>
+      {(formikProps.errors.identificacion && formikProps.touched.identificacion) &&
+        <Text style={styles.errorText}>{formikProps.errors.identificacion}</Text>
+      }
+      <View style={styles.formItem}>
+        <Text style={styles.formLabel}>Nombres</Text>
+        <TextInput
+          style={styles.formInput}
+          name="nombres"
+          onChangeText={formikProps.handleChange('nombres')}
+          onBlur={formikProps.handleBlur('nombres')}
+          value={formikProps.values.nombres}
+          keyboardType="text"
+        />
+      </View>
+      {(formikProps.errors.nombres && formikProps.touched.nombres) &&
+        <Text style={styles.errorText}>{formikProps.errors.nombres}</Text>
+      }
+      <View style={styles.formItem}>
+        <Text style={styles.formLabel}>Asignatura</Text>
+        <TextInput
+          style={styles.formInput}
+          name="asignatura"
+          onChangeText={formikProps.handleChange('asignatura')}
+          onBlur={formikProps.handleBlur('asignatura')}
+          value={formikProps.values.asignatura}
+          keyboardType="text"
+        />
+      </View>
+      {(formikProps.errors.asignatura && formikProps.touched.asignatura) &&
+        <Text style={styles.errorText}>{formikProps.errors.asignatura}</Text>
+      }
+      <View style={styles.formItem}>
+        <Text style={styles.formLabel}>Nota Momento 1 (30%)</Text>
+        <TextInput
+          style={styles.formInput}
+          name="notaUno"
+          onChangeText={formikProps.handleChange('notaUno')}
+          onBlur={formikProps.handleBlur('notaUno')}
+          value={formikProps.values.notaUno}
+          keyboardType="text"
+        />
+      </View>
+      {(formikProps.errors.notaUno && formikProps.touched.notaUno) &&
+        <Text style={styles.errorText}>{formikProps.errors.notaUno}</Text>
+      }
+      <View style={styles.formItem}>
+        <Text style={styles.formLabel}>Nota Momento 2 (35%)</Text>
+        <TextInput
+          style={styles.formInput}
+          name="notaDos"
+          onChangeText={formikProps.handleChange('notaDos')}
+          onBlur={formikProps.handleBlur('notaDos')}
+          value={formikProps.values.notaDos}
+          keyboardType="text"
+        />
+      </View>
+      {(formikProps.errors.notaDos && formikProps.touched.notaDos) &&
+        <Text style={styles.errorText}>{formikProps.errors.notaDos}</Text>
+      }
+      <View style={styles.formItem}>
+        <Text style={styles.formLabel}>Nota Momento 3 (35%)</Text>
+        <TextInput
+          style={styles.formInput}
+          name="notaTres"
+          onChangeText={formikProps.handleChange('notaTres')}
+          onBlur={formikProps.handleBlur('notaTres')}
+          value={formikProps.values.notaTres}
+          keyboardType="text"
+        />
+      </View>
+      {(formikProps.errors.notaTres && formikProps.touched.notaTres) &&
+        <Text style={styles.errorText}>{formikProps.errors.notaTres}</Text>
+      }
+      <View style={styles.formItem}>
+        <Text style={styles.formLabel}>Definitiva</Text>
+        <TextInput
+          style={styles.formInput}
+          value={formikProps.values.definitiva}
+        />
+      </View>
+      <View style={styles.formItem}>
+        <Text style={styles.formLabel}>Observacion</Text>
+        <TextInput
+          style={styles.formInput}
+          name="observacion"
+          onChangeText={formikProps.handleChange('observacion')}
+          onBlur={formikProps.handleBlur('observacion')}
+          value={formikProps.values.observacion}
+          keyboardType="text"
+        />
+      </View>
 
-            <View style={styles.buttons}>
+      <View style={styles.buttons}>
 
-              <TouchableOpacity style={styles.btn} onPress={handleSubmit}>
-                <Text style={styles.btnText}>Calcular / Guardar</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.btn}>
-                <Text style={styles.btnText}>Limpiar</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.btn}>
-                <Text style={styles.btnText}>Buscar</Text>
-              </TouchableOpacity>
-            </View>
-          </>
-        )}
-      </Formik>
+        <TouchableOpacity style={styles.btn} onPress={handleSubmit}>
+          <Text style={styles.btnText}>Calcular / Guardar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={() => formikProps.resetForm()}>
+          <Text style={styles.btnText}>Limpiar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={() => search(formikProps.values.identificacion)}>
+          <Text style={styles.btnText}>Buscar</Text>
+        </TouchableOpacity>
+
+      </View>
     </View>
   );
 }
--- helpers/index.js
+++ helpers/index.js
@@ -1,13 +1,13 @@
+export const init = { identificacion: '', nombres: '', asignatura: '', notaUno: '', notaDos: '', notaTres: '', definitiva: '', observacion: '' }
+
 export const calcularDefinitiva = (notaUno, notaDos, notaTres) => {
-    const definitiva =  (parseFloat(notaUno) + parseFloat(notaDos) + parseFloat(notaTres)) / 3;
-    console.log(definitiva)
-    return definitiva
-}
-
-export const limpiarInputs = () => {
-
+    const definitiva = (parseFloat(notaUno) * 0.3 + parseFloat(notaDos) * 0.35 + parseFloat(notaTres) * 0.35);
+    return definitiva;
 }
 
 export const buscarById = (identificacion) => {
+    const alumnos = JSON.parse(localStorage.getItem('alumnos'))
+    const singleAlumno = alumnos.find(alumno => alumno.identificacion === identificacion)
 
+    return singleAlumno
 }
